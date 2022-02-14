aa403111babda2a09ddd80253c522323e6b1f362
Murphy Fleenor <jmfleeno@gmail.com>
2022-02-13 20:57:20 -0600

    Commiting for Ariticle

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let character = document.getElementById('character');
 let block = document.getElementById('block');
-function jump() {
+const jump = function() {
   if(character.classList !== 'animate'){
     character.classList.add('animate');
   }
@@ -9,6 +9,15 @@ function jump() {
   }, 500);
 }
 
+document.addEventListener('click', jump);
+
 let checkDead = setInterval(function(){
+  // console.log(window.getComputedStyle(c haracter).getPropertyValue('top'));
   let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+  let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+  if(blockLeft<20 && blockLeft > 0 && characterTop >= 130){
+    block.style.animation = 'none';
+    block.style.display = 'none';
+    alert('Game Over');
+  }
 }, 10);
